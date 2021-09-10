ae1378736ce54b8302d47b91109338c97ba43abe
Xabi <xabi.aycaguer@gmail.com>
2021-09-10 15:53:00 +0200

    create jsconfig folder

--- src/App.js
+++ src/App.js
@@ -3,16 +3,15 @@ import { Route, Switch } from 'react-router-dom';
 import Home from './pages/Home';
 
 const App = () => {
-
-
+  /////////////////// Web app /////////////////////////////////////////////////////////////
   //Before install PWA
-  let deferredPrompt
-  window.addEventListener('beforeinstallprompt', (e) => {
+  let deferredPrompt;
+  window.addEventListener("beforeinstallprompt", (e) => {
     // Prevent the mini-infobar from appearing on mobile
     e.preventDefault();
     // Stash the event so it can be triggered later.
     deferredPrompt = e;
-    
+
     // Optionally, send analytics event that PWA install promo was shown.
     console.log(`'beforeinstallprompt' event was fired.`);
   });
@@ -20,7 +19,7 @@ const App = () => {
   //View install PWA
   const installBtn = async () => {
     // Hide the app provided install promotion
-    document.querySelector('.install-app').style.opacity = '0';
+    document.querySelector(".install-app").style.opacity = "0";
     // Show the install prompt
     deferredPrompt.prompt();
     // Wait for the user to respond to the prompt
@@ -28,31 +27,36 @@ const App = () => {
     // Optionally, send analytics event with outcome of user choice
     console.log(`User response to the install prompt: ${outcome}`);
     // We've used the prompt, and can't use it again, throw it away
-    getPWADisplayMode()
+    getPWADisplayMode();
   };
 
   const getPWADisplayMode = () => {
-    const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
-    if (document.referrer.startsWith('android-app://')) {
-      return 'twa';
+    const isStandalone = window.matchMedia(
+      "(display-mode: standalone)"
+    ).matches;
+    if (document.referrer.startsWith("android-app://")) {
+      return "twa";
     } else if (navigator.standalone || isStandalone) {
-      return 'standalone'
-    };
-    return 'browser';
+      return "standalone";
+    }
+    return "browser";
   };
 
-  window.matchMedia('(display-mode: standalone)').addEventListener('change', (evt) => {
-    let displayMode = 'browser';
-    if (evt.matches) {
-      displayMode = 'standalone';
-    }
-    // Log display mode change to analytics
-    console.log('DISPLAY_MODE_CHANGED', displayMode);
-  });
+  window
+    .matchMedia("(display-mode: standalone)")
+    .addEventListener("change", (evt) => {
+      let displayMode = "browser";
+      if (evt.matches) {
+        displayMode = "standalone";
+      }
+      // Log display mode change to analytics
+      console.log("DISPLAY_MODE_CHANGED", displayMode);
+    });
+  /////////////////// Web app /////////////////////////////////////////////////////////////
 
   return (
     <Switch>
-      <Route path='/' component={Home}/>
+      <Route path="/" component={Home} />
     </Switch>
   );
 };
