7b3a513658b8884477062716b32a92d5d3f64da6
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-09-11 05:46:11 +0100

    created login and signup forms f lonutritionist login and signup pages

---
+++ src/components/NutritionistSignupForm.jsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { nutritionistRegisterFetch } from 'services/apiManager';
+import { useState } from 'react';
+import { useDispatch } from "react-redux";
+
+const NutritionistSignupForm = () => {
+
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirmation, setPasswordConfirmation] = useState('');
+  const dispatch = useDispatch();
+
+
+  const handleNutritionistSignup = async (e) => {
+    e.preventDefault();
+    if (email && password && passwordConfirmation) {
+      const nutritionistData = {
+        user: {
+            email: email,
+            password: password,
+            password_confirmation: passwordConfirmation
+        }
+      }
+      await dispatch(nutritionistRegisterFetch(nutritionistData))
+    }
+};
+
+  return (
+    <div className="nutritionist-signup-form">
+      <div className="d-flex justify-content-center">
+        <form className="form-container w-50 " onSubmit={ (e) => handleNutritionistSignup(e) }>
+              <label htmlFor="email" className="text-white">Email*</label>
+              <input type="email" className="form-input-display" placeholder="Votre email"
+              value={email} onChange={ (e) => setEmail(e.target.value) }/>
+              <label htmlFor="password" className="text-white">Mot de passe*</label>
+              <input type="password" className="form-input-display" placeholder="Votre mot de passe"
+              value={password} onChange={ (e) => setPassword(e.target.value) }/>
+              <label htmlFor="password-confirmation" className="text-white">Confirmation de mot de passe*</label>
+              <input type="password" className="form-input-display" placeholder="Confirmez votre mot de passe"
+              value={passwordConfirmation} onChange={ (e) => setPasswordConfirmation(e.target.value) }/>
+          <input type="submit" className="btn btn-light register-btn mt-5 w-100" value="S'enregistrer" />
+        </form>
+      </div>
+    </div>
+
+  );
+};
+
+export default NutritionistSignupForm;
--- src/pages/SignupNutritionist.jsx
+++ src/pages/SignupNutritionist.jsx
@@ -1,9 +1,14 @@
+import NutritionistSignupForm from 'components/NutritionistSignupForm';
 import React from 'react';
 
 const SignupNutritionist = () => {
   return (
     <div className="signup-nutritionist">
-      <h1>Signup Nutritionist</h1>
+      <div className="default-background d-flex align-items-center py-5">
+        <div className="d-flex justify-content-center col-12 my-5">
+          <NutritionistSignupForm />
+        </div>
+      </div>
     </div>
   );
 };
