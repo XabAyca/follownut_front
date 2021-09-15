a6f84603b687ceed5ba8c3e2a05d9babdd4e4b26
Anthony Lebro <antho.devjam@gmail.com>
2021-09-15 15:52:03 +0200

    404

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
     <div className="not-found">
-      <h1>404 ERROR</h1>
-      <h3>Page not found</h3>
+      <h1 className="title-not-found">Erreur 404</h1>
+      <div className="div-not-found">
+        <h3>Désolé mais la page que vous cherchez n'existe pas...</h3>
+        <Link to ='/'> <button className="d-flex justify-content-center mt-2 btn secondary-button w-50">
+            Retour l'accueil
+          </button></Link>
+      </div>
     </div>
   );
 };
