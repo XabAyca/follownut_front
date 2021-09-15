81e3c80ad9b0978651f116395ed764b2929f3ca7
Augustin <augustin.berne.pro@gmail.com>
2021-09-15 15:38:11 +0200

    Removing useless files

--- src/pages/Auth/LoginNutritionist.jsx
+++ src/pages/Auth/LoginNutritionist.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 const LoginNutritionist = () => {
   return (
     <div className='login-nutritionist'>
-      <div className="default-background d-flex align-items-center py-5">
+      <div className="jumbotron-auth d-flex align-items-center py-5">
         <div className="d-flex justify-content-center col-12 my-5 row">
           <h1 className="d-flex justify-content-center text-white my-2">Bon retour parmi nous !</h1>
           <NutritionistLoginForm />
