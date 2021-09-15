bd9cd3bc3c38946b7d122b48e070fb5204d4a19e
Augustin <augustin.berne.pro@gmail.com>
2021-09-15 16:24:49 +0200

    Finishing design of auth pages

--- src/pages/Auth/ResetPasswordNutritionist.jsx
+++ src/pages/Auth/ResetPasswordNutritionist.jsx
@@ -4,7 +4,8 @@ const ResetPasswordNutritionist = () => {
   return (
     <div className="forgotten-password-nutritionist">
       <div className="jumbotron-auth d-flex align-items-center py-5">
-        <div className="d-flex justify-content-center col-12 my-5">
+        <div className="d-flex justify-content-center col-12 my-5 row">
+        <h1 className="d-flex justify-content-center text-primary-color my-4">Choisissez votre nouveau mot de passe</h1>
           <NutritionistPasswordResetForm/>
         </div>
       </div>
