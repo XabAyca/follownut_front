8d8551d73158c959e06a0fd9a0b6b8477ed9c6c6
Augustin <augustin.berne.pro@gmail.com>
2021-09-15 15:05:47 +0200

    Views for log in are ok

--- src/pages/Auth/LoginPatient.jsx
+++ src/pages/Auth/LoginPatient.jsx
@@ -5,7 +5,8 @@ const SignupPatient = () => {
   return (
     <div className="login-patient">
       <div className="default-background d-flex align-items-center py-5">
-        <div className="d-flex justify-content-center col-12 my-5">
+        <div className="d-flex justify-content-center col-12 my-5 row">
+          <h1 className="d-flex justify-content-center text-white my-2">Bon retour parmi nous !</h1>
           <PatientLoginForm />
         </div>
       </div>
