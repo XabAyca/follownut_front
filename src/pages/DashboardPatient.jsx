f4cc6e1629dfe3b31a115a84bedd29bdf7541011
kleberkunha <kleberkunha@hotmail.com>
2021-09-16 09:50:43 +0200

    updating patient-Dashbard

--- src/pages/DashboardPatient.jsx
+++ src/pages/DashboardPatient.jsx
@@ -5,50 +5,55 @@ import LeftMenu from "components/LeftMenu";
 const DashboardPatient = () => {
   return (
     <>
-    <div className="container-fluid d-flex">
+  
+
+
+    <div className="background-of-dashboard-patient">
       <LeftMenu/>
 
-      <div className="container">
-        <div className="row data-patient">
-          <div className="col">
-            <h5>Mathieu Paradis</h5>
-            <h5>Mathieu.Paradis@gmail.com</h5>
-            <h5>Height : 1.78</h5>
-          </div>
-          <div className="col">
-              <h5>Current Weight : 82Kg</h5>
-              <h5>Current Body Fat : 82Kg</h5>
-              <h5>Visceral Fat : 82Kg</h5>
-              <h5>BMI : 82Kg</h5>
+      <div className="container rounded bg-white mb-5">
+        <div className="row">
+          <div className="col-md-6 border-right">
+            <div className="d-flex flex-column p-3 py-5">
+              <h6>Mathieu Paradis</h6>
+              <h6>Mathieu.Paradis@gmail.com</h6>
+              <h6>Height : 1.78</h6>
             </div>
+          </div>
+          <div className="col-md-6 border-right ">
+            <div className="d-flex flex-column justify-content-center align-items-center p-3 py-5">
+              <h6>Current Weight : 82Kg</h6>
+              <h6>Current Body Fat : 82Kg</h6>
+              <h6>Visceral Fat : 82Kg</h6>
+              <h6>BMI : 82Kg</h6>
+            </div>
+          </div>
         </div>
-        <div className="container">
-          <div className="row mt-5">
-            <div className="col d-flex justify-content-center">
-              <div className="w-75">
-                <h3>Weight</h3>
-                <GraphWeight/>
-              </div>
-            </div>
-            <div className="col d-flex justify-content-center">
-              <div className="w-75">
-                <h3>Body Fat</h3>
-                <GraphWeight/>
-              </div>
+      </div>
+      <div className="container rounded bg-white ">
+        <div className="row">
+          <div className="col-md-3 border-right ">
+            <div className="d-flex flex-column  p-3 py-5">
+              <h3>Weight</h3>
+              <GraphWeight/>
             </div>
           </div>
-          <div className="row mt-5">
-            <div className="col d-flex justify-content-center">
-              <div className="w-75">
-              <h3>Viceral Fat</h3>
-                <GraphWeight/>
-              </div>
+          <div className="col-md-3 border-right ">
+            <div className="d-flex flex-column  p-3 py-5">
+              <h3>Body Fat</h3>
+              <GraphWeight/>
             </div>
-            <div className="col d-flex justify-content-center">
-              <div className="w-75">
+          </div>
+          <div className="col-md-3 border-right ">
+            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+              <h3>Viceral Fat</h3>
+              <GraphWeight/>
+            </div>
+          </div>
+          <div className="col-md-3 border-right ">
+            <div className="d-flex flex-column align-items-center text-center p-3 py-5">
               <h3>Muscle Mass</h3>
-                <GraphWeight/>
-              </div>
+              <GraphWeight/>
             </div>
           </div>
         </div>
