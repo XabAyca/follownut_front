57065ae9825e5ca8996619591645103934011ef2
kleberkunha <kleberkunha@hotmail.com>
2021-09-16 09:02:42 +0200

    PatientProfile adding the same view as PatientEditProfile

--- src/pages/PatientProfile.jsx
+++ src/pages/PatientProfile.jsx
@@ -30,30 +30,60 @@ const PatientProfile = () => {
   console.log(patient ? patient : "test")
 
   return (
-    <div className="patient-profile">
-      <h1>Profil du patient</h1>
-      <p>
-        <strong>Prenom : </strong>
-        {
-          patient ? 
-          patient.first_name : <Loading color={"blue"} />
-        }
-      </p>
-      <p>
-        <strong>Nom : </strong>
-        {
-          patient ? 
-          patient.last_name : <Loading color={"blue"} />
-        }
-      </p>
-      <p>
-        <strong>Email : </strong> 
-        {
-          patient ? 
-          patient.email : <Loading color={"blue"} />
-        } 
-      </p>
-    </div>
+    <>
+      <div className="background-of-edit-profile">
+        <div className="container rounded bg-white mb-5">
+          <div className="row">
+            <div className="col-md-3 border-right">
+              <div className="d-flex flex-column align-items-center text-center p-3 py-5">
+                <img className="rounded-circle mt-5" width="150px" 
+                src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg"
+                alt="profile"/><span className="font-weight-bold">Edogaru</span>
+                <span className="text-black-50">edogaru@mail.com.my</span>
+              </div>
+            </div>
+            <div className="col-md-5 border-right">
+              <div className="p-3">
+                <div className=" mb-3">
+                    <h4 className="text-right">Patient Profile</h4>
+                </div>
+                <div className="row mt-2">
+                    <div className="col-md-6">
+                      <p>
+                        <strong>Prenom : </strong>
+                        {
+                          patient ? 
+                          patient.first_name : <Loading color={"blue"} />
+                        }
+                      </p>
+                    </div>
+                    <div className="col-md-6">
+                      <p>
+                        <strong>Nom : </strong>
+                        {
+                          patient ? 
+                          patient.last_name : <Loading color={"blue"} />
+                        }
+                      </p>
+                    </div>
+                </div>
+                <div className="row mt-3">
+                  <div className="col-md-12">
+                    <p>
+                      <strong>E-mail : </strong>
+                      {
+                        patient ? 
+                        patient.email : <Loading color={"blue"} />
+                      } 
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
