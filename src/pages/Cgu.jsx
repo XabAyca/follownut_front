1216aad9992bc5201d1901b674b95276a1a4f823
Xabi <xabi.aycaguer@gmail.com>
2021-09-18 12:37:57 +0200

    create CGU page

--- src/pages/Cgu.jsx
+++ src/pages/Cgu.jsx
@@ -6,138 +6,134 @@ const Cgu = () => {
       <h1>Conditions générales d'utilisation du site FOLLOWNUT'</h1>
 
 
-<h2>ARTICLE 1 : Objet</h2>
+      <h2>ARTICLE 1 : Objet</h2>
 
-<p>Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique de l’utilisation du site [votre site] et de ses services.</p>
+      <p>Les présentes « conditions générales d'utilisation » ont pour objet l'encadrement juridique de l’utilisation du site [votre site] et de ses services.</p>
 
-<p>Ce contrat est conclu entre :</p>
+      <p>Ce contrat est conclu entre :</p>
 
-<p>Le gérant du site internet, ci-après désigné « l’Éditeur »,</p>
+      <p>Le gérant du site internet, ci-après désigné « l’Éditeur »,</p>
 
-<p>Toute personne physique ou morale souhaitant accéder au site et à ses services, ci-après appelé « l’Utilisateur ».</p>
+      <p>Toute personne physique ou morale souhaitant accéder au site et à ses services, ci-après appelé « l’Utilisateur ».</p>
 
-<p>Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acceptation de ces conditions.</p>
+      <p>Les conditions générales d'utilisation doivent être acceptées par tout Utilisateur, et son accès au site vaut acceptation de ces conditions.</p>
 
 
-<h2>ARTICLE 2 : Mentions légales</h2>
+      <h2>ARTICLE 2 : Mentions légales</h2>
 
-<p>Pour les personnes morales :</p>
+      <p>Pour les personnes morales :</p>
 
-<p>Le site [nom du site] est édité par la société [nom de la société], [statut juridique (SAS, SARL, etc.)] au capital de [montant en euros] €, dont le siège social est situé au [adresse du siège social].</p>
+      <p>Le site [nom du site] est édité par la société [nom de la société], [statut juridique (SAS, SARL, etc.)] au capital de [montant en euros] €, dont le siège social est situé au [adresse du siège social].</p>
 
-<p>La société est représentée par [nom et prénom du responsable].</p>
+      <p>La société est représentée par [nom et prénom du responsable].</p>
 
 
-<p>Pour les personnes physiques : </p>
+      <p>Pour les personnes physiques : </p>
 
-<p>Le site [nom du site] est édité par [nom et prénom du responsable], domicilié au [adresse postale].</p>
+      <p>Le site [nom du site] est édité par [nom et prénom du responsable], domicilié au [adresse postale].</p>
 
 
 
-<h2>ARTICLE 3 : accès aux services</h2>
+      <h2>ARTICLE 3 : accès aux services</h2>
 
-L’Utilisateur du site [votre site] a accès aux services suivants :
-[Service n°1]
-[Service n°2]
-[Service n°3]
-[Service n°4]
-…
+      <p>L’Utilisateur du site [votre site] a accès aux services suivants :</p>
+      [Service n°1]
+      …
 
-Tout Utilisateur ayant accès a internet peut accéder gratuitement et depuis n’importe où au site. Les frais supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la charge de l’Éditeur.
+      <p>Tout Utilisateur ayant accès a internet peut accéder gratuitement et depuis n’importe où au site. Les frais supportés par l’Utilisateur pour y accéder (connexion internet, matériel informatique, etc.) ne sont pas à la charge de l’Éditeur.</p>
 
-Les services suivants ne sont pas accessible pour l’Utilisateur que s’il est membre du site (c’est-à-dire qu’ile st identifié à l’aide de ses identifiants de connexion) :
-[Service n°1]
-[Service n°2]
-…
+      <p>Les services suivants ne sont pas accessible pour l’Utilisateur que s’il est membre du site (c’est-à-dire qu’ile st identifié à l’aide de ses identifiants de connexion) :</p>
+      [Service n°1]
+      …
 
-Le site et ses différents services peuvent être interrompus ou suspendus par l’Éditeur, notamment à l’occasion d’une maintenance, sans obligation de préavis ou de justification.
+      <p>Le site et ses différents services peuvent être interrompus ou suspendus par l’Éditeur, notamment à l’occasion d’une maintenance, sans obligation de préavis ou de justification.</p>
 
 
-<h2>ARTICLE 4 : Responsabilité de l’Utilisateur</h2>
+      <h2>ARTICLE 4 : Responsabilité de l’Utilisateur</h2>
 
-L'Utilisateur est responsable des risques liés à l’utilisation de son identifiant de connexion et de son mot de passe. 
+      <p>L'Utilisateur est responsable des risques liés à l’utilisation de son identifiant de connexion et de son mot de passe.</p>
 
-Le mot de passe de l’Utilisateur doit rester secret. En cas de divulgation de mot de passe, l’Éditeur décline toute responsabilité.
+      <p>Le mot de passe de l’Utilisateur doit rester secret. En cas de divulgation de mot de passe, l’Éditeur décline toute responsabilité.</p>
 
-L’Utilisateur assume l’entière responsabilité de l’utilisation qu’il fait des informations et contenus présents sur le site [votre site].
+      <p>L’Utilisateur assume l’entière responsabilité de l’utilisation qu’il fait des informations et contenus présents sur le site [votre site].</p>
 
-Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.
+      <p>Tout usage du service par l'Utilisateur ayant directement ou indirectement pour conséquence des dommages doit faire l'objet d'une indemnisation au profit du site.</p>
 
-Le site permet aux membres de publier sur le site :
-[Commentaires] ;
-[Oeuvres] ;
-Etc.
+      <p>Le site permet aux membres de publier sur le site :</p>
+      <p>[Commentaires] ;</p>
+      <p>[Oeuvres] ;</p>
+      <p>Etc.</p>
 
-Le membre s’engage à tenir des propos respectueux des autres et de la loi et accepte que ces publications soient modérées ou refusées par l’Éditeur, sans obligation de justification. 
+      <p>Le membre s’engage à tenir des propos respectueux des autres et de la loi et accepte que ces publications soient modérées ou refusées par l’Éditeur, sans obligation de justification. </p>
 
-En publiant sur le site, l’Utilisateur cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé.
+      <p>En publiant sur le site, l’Utilisateur cède à la société éditrice le droit non exclusif et gratuit de représenter, reproduire, adapter, modifier, diffuser et distribuer sa publication, directement ou par un tiers autorisé.</p>
 
-L’Éditeur s'engage toutefois à citer le membre en cas d’utilisation de  sa publication
+      <p>L’Éditeur s'engage toutefois à citer le membre en cas d’utilisation de  sa publication</p>
 
 
 
-<h2>ARTICLE 5 : Responsabilité de l’Éditeur</h2>
+      <h2>ARTICLE 5 : Responsabilité de l’Éditeur</h2>
 
-Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité de l’Éditeur.
+      <p>Tout dysfonctionnement du serveur ou du réseau ne peut engager la responsabilité de l’Éditeur.</p>
 
-De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.
+      <p>De même, la responsabilité du site ne peut être engagée en cas de force majeure ou du fait imprévisible et insurmontable d'un tiers.</p>
 
-Le site [votre site] s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.
+      <p>Le site [votre site] s'engage à mettre en œuvre tous les moyens nécessaires pour garantir la sécurité et la confidentialité des données. Toutefois, il n’apporte pas une garantie de sécurité totale.</p>
 
-L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations diffusées su le site soient réputées fiables.
+      <p>L’Éditeur se réserve la faculté d’une non-garantie de la fiabilité des sources, bien que les informations diffusées su le site soient réputées fiables.</p>
 
-<h2>ARTICLE 6 : Propriété intellectuelle</h2>
+      <h2>ARTICLE 6 : Propriété intellectuelle</h2>
 
-Les contenus du site [votre site] (logos, textes, éléments graphiques, vidéos, etc.) son protégés par le droit d’auteur, en vertu du Code de la propriété intellectuelle.
+      <p>Les contenus du site [votre site] (logos, textes, éléments graphiques, vidéos, etc.) son protégés par le droit d’auteur, en vertu du Code de la propriété intellectuelle.</p>
 
-L’Utilisateur devra obtenir l’autorisation de l’éditeur du site avant toute reproduction, copie ou publication de ces différents contenus.
+      <p>L’Utilisateur devra obtenir l’autorisation de l’éditeur du site avant toute reproduction, copie ou publication de ces différents contenus.</p>
 
-Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.
+      <p>Ces derniers peuvent être utilisés par les utilisateurs à des fins privées ; tout usage commercial est interdit.</p>
 
-L’Utilisateur est entièrement responsable de tout contenu qu’il met en ligne et il s’engage à ne pas porter atteinte à un tiers.
+      <p>L’Utilisateur est entièrement responsable de tout contenu qu’il met en ligne et il s’engage à ne pas porter atteinte à un tiers.</p>
 
-L’Éditeur du site se réserve le droit de modérer ou de supprimer librement et à tout moment les contenus mis en ligne par les utilisateurs, et ce sans justification.
+      <p>L’Éditeur du site se réserve le droit de modérer ou de supprimer librement et à tout moment les contenus mis en ligne par les utilisateurs, et ce sans justification.</p>
 
 
-<h2>ARTICLE 7 : Données personnelles</h2>
+      <h2>ARTICLE 7 : Données personnelles</h2>
 
-L’Utilisateur doit obligatoirement fournir des informations personnelles pour procéder à son inscription sur le site. 
+      <p>L’Utilisateur doit obligatoirement fournir des informations personnelles pour procéder à son inscription sur le site. </p>
 
-L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site [nom de votre site] pour la communication d’informations diverses et la gestion du compte.
+      <p>L’adresse électronique (e-mail) de l’utilisateur pourra notamment être utilisée par le site [nom de votre site] pour la communication d’informations diverses et la gestion du compte.</p>
 
-[Votre site] garantie le respect de la vie privée de l’utilisateur, conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.
+      <p>[Votre site] garantie le respect de la vie privée de l’utilisateur, conformément à la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux fichiers et aux libertés.</p>
 
-Le site est déclaré auprès de la CNIL sous le numéro suivant : [numéro].
+      <p>Le site est déclaré auprès de la CNIL sous le numéro suivant : [numéro].</p>
 
-En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :
+      <p>En vertu des articles 39 et 40 de la loi en date du 6 janvier 1978, l'Utilisateur dispose d'un droit d'accès, de rectification, de suppression et d'opposition de ses données personnelles. L'Utilisateur exerce ce droit via :</p>
 
-Son espace personnel sur le site ;
-Un formulaire de contact ;
-Par mail à [adresse mail de l’administrateur] ;
-Par voie postale au [votre adresse].
+      <p>Son espace personnel sur le site ;</p>
+      <p>Un formulaire de contact ;</p>
+      <p>Par mail à [adresse mail de l’administrateur] ;</p>
+      <p>Par voie postale au [votre adresse].</p>
 
 
-<h2>ARTICLE 8 : Liens hypertextes</h2>
+      <h2>ARTICLE 8 : Liens hypertextes</h2>
 
-Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité de l’Éditeur de [votre site], qui n’a pas de contrôle sur ces liens.
+      <p>Les domaines vers lesquels mènent les liens hypertextes présents sur le site n’engagent pas la responsabilité de l’Éditeur de [votre site], qui n’a pas de contrôle sur ces liens.</p>
 
-Il est possible pour un tiers de créer un lien vers une page du site [votre site] sans autorisation expresse de l’éditeur.
+      <p>Il est possible pour un tiers de créer un lien vers une page du site [votre site] sans autorisation expresse de l’éditeur.</p>
 
 
-<h2>ARTICLE 9 : Évolution des conditions générales d’utilisation</h2>
+      <h2>ARTICLE 9 : Évolution des conditions générales d’utilisation</h2>
 
-Le site [votre site] se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.
+      <p>Le site [votre site] se réserve le droit de modifier les clauses de ces conditions générales d’utilisation à tout moment et sans justification.</p>
 
 
-<h2>ARTICLE 10 : Durée du contrat</h2>
+      <h2>ARTICLE 10 : Durée du contrat</h2>
 
-La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter du début de l’utilisation du service.
+      <p>La durée du présent contrat est indéterminée. Le contrat produit ses effets à l'égard de l'Utilisateur à compter du début de l’utilisation du service.</p>
 
 
-<h2>ARTICLE 11 : Droit applicable et juridiction compétente</h2>
+      <h2>ARTICLE 11 : Droit applicable et juridiction compétente</h2>
 
-Le présent contrat dépend de la législation française. 
-En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur, les tribunaux de [nom de ville] sont compétents pour régler le contentieux.
+      <p>Le présent contrat dépend de la législation française.
+        En cas de litige non résolu à l’amiable entre l’Utilisateur et l’Éditeur, les tribunaux de [nom de ville] sont compétents pour régler le contentieux.</p>
 
     </div>
   );
