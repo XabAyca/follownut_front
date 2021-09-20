bb02bad7f893881157dedeb9a0d223787adb3f71
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-09-20 19:32:52 +0100

    updated lgobook list

--- src/components/LogbooksList.jsx
+++ src/components/LogbooksList.jsx
@@ -44,42 +44,46 @@ const LogbooksList = ({ filteredLogbooks, setOpenModal, setOpenCreateModal, setO
           </div>
         </div>
         <div className="details-container p-3">
-        <table className="table patient-table">
-          <thead>
-            <tr>
-              <th scope="col">Réf.</th>
-              <th scope="col">Date</th>
-              <th scope="col">Titre</th>
-              <th scope="col">Public</th>
-              <th scope="col"></th>
-              <th scope="col"></th>
-              <th scope="col"></th>
-            </tr>
-          </thead>
-          <tbody>
           {
-              logbooks.map((logbook) => {
-                return (
-                  <tr>
-                    <th scope="row" key={logbook.id}>{logbook.id}</th>
-                    <td>{createDate(logbook.updated_at)}</td>
-                    <td>{logbook.title}</td>
-                    <td>{logbook.is_shared ? "oui" : "non"}</td>
-                    <td>
-                      <i className="pointer-clickable far fa-eye" onClick={() => setOpenModal(logbook)}></i>
-                    </td>
-                    <td>
-                      <i className="pointer-clickable fas fa-pencil-alt" onClick={() => setOpenUpdateModal(logbook)}></i>
-                    </td>
-                    <td>
-                      <i className="pointer-clickable fas fa-trash-alt" onClick={(e) => deleteLogbook(logbook, e)}></i>
-                    </td>
-                  </tr>
-                )
-              })
-            }
-          </tbody>
-        </table>
+            logbooks.length > 0 ? 
+            <table className="table patient-table">
+            <thead>
+              <tr>
+                <th scope="col">Réf.</th>
+                <th scope="col">Date</th>
+                <th scope="col">Titre</th>
+                <th scope="col">Public</th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+                <th scope="col"></th>
+              </tr>
+            </thead>
+            <tbody>
+            {
+                logbooks.map((logbook) => {
+                  return (
+                    <tr>
+                      <th scope="row" key={logbook.id}>{logbook.id}</th>
+                      <td>{createDate(logbook.updated_at)}</td>
+                      <td>{logbook.title}</td>
+                      <td>{logbook.is_shared ? "oui" : "non"}</td>
+                      <td>
+                        <i className="pointer-clickable far fa-eye" onClick={() => setOpenModal(logbook)}></i>
+                      </td>
+                      <td>
+                        <i className="pointer-clickable fas fa-pencil-alt" onClick={() => setOpenUpdateModal(logbook)}></i>
+                      </td>
+                      <td>
+                        <i className="pointer-clickable fas fa-trash-alt" onClick={(e) => deleteLogbook(logbook, e)}></i>
+                      </td>
+                    </tr>
+                  )
+                })
+              }
+            </tbody>
+          </table>
+          : <h3>Vous n'avez pas encore de notes</h3>
+          }
         </div>
       </div>
     </div>
