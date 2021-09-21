470c4c0ea587e2cb724fe97462790663d4b66144
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-09-21 03:52:48 +0100

    updated nutrionists page

--- src/pages/Nutritionists.jsx
+++ src/pages/Nutritionists.jsx
@@ -7,12 +7,11 @@ const Nutritionists = () => {
   const [nutritionistsPartenaires, setNutritionistsPartenaires] = useState("");
   const [filter, setFilter] = useState("");
  
-  const nutritionists = useSelector(state => state.nutritionists.nutritionist)
-  const dispatch = useDispatch() 
+  const nutritionists = useSelector(state => state.nutritionists.nutritionist);
+  const dispatch = useDispatch();
 
   const getFilterNutritionists = () => {
     if (nutritionists) {
-      console.log(nutritionists)
       setNutritionistsPartenaires(nutritionists.filter((nutritionist) => {
         if (nutritionist.first_name && nutritionist.last_name) {
           return (
@@ -35,14 +34,16 @@ const Nutritionists = () => {
 
   useEffect(() => {
     getFilterNutritionists();
-    console.log(nutritionistsPartenaires)
   }, [filter, nutritionists]);
 
   return (
     <div className="page-padding">
-      <div className="search-input col-sm-5 col-md-4 col-lg-5 col-xl-3 d-flex">
-        <i className="fas fa-search"></i>
-        <input type="text" placeholder="    Recherche..." onChange={(e) => setFilter(e.target.value.toLowerCase())} />
+      <div className="mx-4">
+        <div className="search-input my-3 col-sm-5 col-md-4 col-lg-5 col-xl-3 d-flex">
+          <i className="fas fa-search"></i>
+          <input type="text" placeholder="    Recherche..." onChange={(e) => setFilter(e.target.value.toLowerCase())} />
+        </div>
+        <h2>Voici les nutritionistes utilisant notre application</h2>
       </div>
       <div className=" d-flex flex-wrap">
         { nutritionistsPartenaires &&
