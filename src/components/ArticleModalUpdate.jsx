dea30699379115cdf711650f27ff836d17fc13af
Augustin <augustin.berne.pro@gmail.com>
2021-09-22 17:12:36 +0200

    Edit modal for article is set up

--- src/components/ArticleModalUpdate.jsx
+++ src/components/ArticleModalUpdate.jsx
@@ -32,29 +32,26 @@ const ArticleModalUpdate = ({article}) => {
   }
 
   return (
+
     <div className="update-article-modal">
       <i className="pointer-clickable fas fa-times" onClick={closeModal}></i>
-
       <div className="content p-3 ">
-          <div className="p-3">
-          {article && 
-
-            <form className="col-md-5 border-right" onSubmit={updatePublication}>
-              <div className="d-flex flex-column">
-                <label className="labels">Titre</label>
-                <input type="text" className="form-input-display" placeholder={titleArticle} value={titleArticle} onChange={(e) => setTitleArticle(e.target.value)} required/>
+      {article &&
+        <div className="p-3">
+          <h1 className="text-primary-color">Editer un article</h1>
+          <form className="appointment-form text-primary-color" onSubmit={updatePublication}>
+            <div className="d-flex row justify-content-between py-2">
+              <div className="col-lg-6 col-sm-12 my-2">
+                <label>Titre</label>
+                <input className="input-display" type="text" id="article-title" placeholder={titleArticle} value={titleArticle} onChange={(e) => setTitleArticle(e.target.value)} required />
               </div>
-              <div className="d-flex flex-column mt-4">
-                <label className="labels">Contenu</label>
-                <input type="text" className="form-input-display" placeholder={contentArticle} value={contentArticle} onChange={(e) => setContentArticle(e.target.value)} required/>
-              </div>
-
-              <div className="mt-5 text-center">
-                <input type="submit" value="Sauvegarder" className="btn success-button text-center patient-edit-profile-button w-100 mt-4" />
-              </div>
-            </form>
-          }
-          </div>
+            </div>
+            <label>Corps de l'article</label>
+            <textarea id="article-content" className="input-display textarea-display" placeholder={contentArticle} value={contentArticle} onChange={(e) => setContentArticle(e.target.value)} required />
+            <input type="submit" value="Sauvegarder" className="btn success-button my-3 col-lg-3 col-sm-12" />
+          </form>
+        </div>
+      }
       </div>
     </div>
   );
