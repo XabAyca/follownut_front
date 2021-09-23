d3404591b4c79eec0c9c63b4d6ff8ac0ebf2083e
Augustin <augustin.berne.pro@gmail.com>
2021-09-23 19:19:45 +0200

    patient profile is done

--- src/pages/PatientEditProfile.jsx
+++ src/pages/PatientEditProfile.jsx
@@ -36,21 +36,19 @@ const PatientEditProfile = () => {
   },[patients]);
 
   return (
-    <>
-      <div className="background-of-edit-profile page-padding">
-        <div className="container rounded bg-white content-edit-profile">
-          <div className="row">
-            <div className="col-md-3 border-right">
-              <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" alt="profile"/></div>
-            </div>
-            {
-              (patient && nutritionists)?
-              <PatientEditForm patientData={patient} nutritionists={nutritionists}/> : <Loading />
-            }
+    <div className="background-of-edit-profile page-padding">
+      <div className="container rounded bg-white content-edit-profile">
+        <div className="row">
+          <div className="col-md-3 border-right">
+            <div className="d-flex flex-column align-items-center text-center p-3 py-5"><img className="rounded-circle mt-5" width="150px" src="https://st3.depositphotos.com/15648834/17930/v/600/depositphotos_179308454-stock-illustration-unknown-person-silhouette-glasses-profile.jpg" alt="profile"/></div>
           </div>
+          {
+            (patient && nutritionists)?
+            <PatientEditForm patientData={patient} nutritionists={nutritionists}/> : <Loading />
+          }
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
