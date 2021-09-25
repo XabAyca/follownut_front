45b8fb106b602a2d3b0503862355f15a5e20a2eb
Augustin <augustin.berne.pro@gmail.com>
2021-09-25 15:44:25 +0200

    Responsive for static pages is done

--- src/components/Forms/PatientSignupForm.jsx
+++ src/components/Forms/PatientSignupForm.jsx
@@ -36,7 +36,7 @@ const PatientSignupForm = () => {
   return (
     <div className="patient-signup-form d-flex justify-content-center">
       <div className="form-container">
-      <h5 className="d-flex justify-content-center text-third-color pb-3">Formulaire d'inscription pour les patients</h5>
+      <h5 className="d-flex justify-content-center text-third-color">Inscription patient</h5>
         <form onSubmit={ (e) => handlePatientSignup(e) }>
           <label htmlFor="email" className="text-white-color pt-2">Email*</label>
           <input type="email" className="input-display" placeholder="Votre email"
@@ -47,13 +47,15 @@ const PatientSignupForm = () => {
           <label htmlFor="password-confirmation" className="text-white-color pt-2">Confirmation de mot de passe*</label>
           <input type="password" className="input-display" placeholder="Confirmez votre mot de passe"
           value={passwordConfirmation} onChange={ (e) => setPasswordConfirmation(e.target.value) }/>
-          <input type="submit" className="btn success-button register-btn mt-5 w-100" value="S'enregistrer" />
-        </form>
-        <Link to="/login-patient">
-          <div className="d-flex justify-content-center text-white-color mt-2 w-100">
-            Se connecter
+          <div className="my-4">
+            <input type="submit" className="btn success-button register-btn w-100" value="S'enregistrer" /> 
+            <Link to="/login-patient">
+              <div className="d-flex justify-content-center my-1 text-white-color w-100">
+                Se connecter
+              </div>
+            </Link>
           </div>
-        </Link>
+        </form>
       </div>
     </div>
   );
