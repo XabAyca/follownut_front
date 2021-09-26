672d6b6ffa75d0aa3c57a8d11b8020c101e5d101
MathieuParadis <mathieuyl.paradis@gmail.com>
2021-09-26 20:21:18 +0100

    modified nutritionist card component

--- src/components/NutritionistCard.jsx
+++ src/components/NutritionistCard.jsx
@@ -6,29 +6,24 @@ const NutritionistCard = ({ nutritionistData }) => {
   const defaultAvatar = "https://cdn.pixabay.com/photo/2013/07/13/12/07/avatar-159236_1280.png";
   const history = useHistory()
   const isPatient = Cookies.get("patient_token_cookie");
+  const isNutritionist = Cookies.get('nutritionist_id_cookie');
+
 
   return (
     <div className="basic-card">
       <div className="img-box">
-        <img
-          className="card-img-top"
-          src={avatar ? avatar : defaultAvatar}
-          alt="Card image cap"
-        />
+        <img className="card-img-top" src={avatar ? avatar : defaultAvatar} alt="Card image cap" />
       </div>
       <div className="card-body">
-        <h3>
-          {first_name} {last_name}
-        </h3>
+        <h3>{first_name} {last_name}</h3>
         <p>Email : {email}</p>
         <p>Tel : {phone_number}</p>
       </div>
-      <p
-        className="take-appointment btn  third-button"
-        onClick={()=>history.push(isPatient? "/patient-dashboard":"/signup-patient")}
-      >
-        Prendre rendez-vous
-      </p>
+      { !isPatient && !isNutritionist && 
+        <p className="take-appointment btn  third-button" onClick={()=>history.push("/signup-patient")}>
+          Prendre rendez-vous
+        </p>
+      }
     </div>
   );
 };
