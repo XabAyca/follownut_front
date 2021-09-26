b8bcf028d392d22eff3831f51abacaef0c194c6e
Augustin <augustin.berne.pro@gmail.com>
2021-09-26 18:15:30 +0200

    Create compte-rendu is ok

--- src/components/Forms/AppointmentCreateForm.jsx
+++ src/components/Forms/AppointmentCreateForm.jsx
@@ -73,7 +73,7 @@ const AppointmentCreateForm = () => {
         onSubmit={createDataAppointment}
       >
         <div className="row">
-          <div className="col-lg-3 col-sm-8">
+          <div>
             <select
               name="patient"
               id="patient_id"
@@ -100,7 +100,7 @@ const AppointmentCreateForm = () => {
         </div>
 
         <div className="d-flex row justify-content-between py-2">
-          <div className="col-lg-2 col-sm-8">
+          <div className="col-lg-3 col-sm-12">
             <label>Graisse viscérale</label>
             <input
               className="input-display"
@@ -112,7 +112,7 @@ const AppointmentCreateForm = () => {
               required
             />
           </div>
-          <div className="col-lg-2 col-sm-8">
+          <div className="col-lg-3 col-sm-12">
             <label>Masse musculaire</label>
             <input
               className="input-display"
@@ -124,7 +124,7 @@ const AppointmentCreateForm = () => {
               required
             />
           </div>
-          <div className="col-lg-2 col-sm-8">
+          <div className="col-lg-2 col-sm-12">
             <label>Masse grasse</label>
             <input
               className="input-display"
@@ -136,7 +136,7 @@ const AppointmentCreateForm = () => {
               required
             />
           </div>
-          <div className="col-lg-2 col-sm-8">
+          <div className="col-lg-2 col-sm-12">
             <label>Taille</label>
             <input
               className="input-display"
@@ -149,7 +149,7 @@ const AppointmentCreateForm = () => {
               required
             />
           </div>
-          <div className="col-lg-2 col-sm-8">
+          <div className="col-lg-2 col-sm-12">
             <label>Poids</label>
             <input
               className="input-display"
